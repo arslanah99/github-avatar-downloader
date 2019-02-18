090c81465debbd9ed3fd3f6a656a2509636760b7
arslanah99 <Arah09@hotmail.com>
2019-02-18 06:21:13 +0000

    fully linted and no dead code

--- download_avatars.js
+++ download_avatars.js
@@ -1,37 +1,39 @@
-var request = require('request');
-var getToken = require('./secrets')
-var fs = require('fs');
+var request = require("request");
+var getToken = require("./secrets");
+var fs = require("fs");
 var owner = process.argv[2];
 var repoName = process.argv[3];
 function getRepoContributors(owner, repoName, cb) {
-    var options ={
-    url :'https://api.github.com/repos/' + owner + '/' + repoName + '/contributors',
+  var options = {
+    url:
+      "https://api.github.com/repos/" +
+      owner +
+      "/" +
+      repoName +
+      "/contributors",
     headers: {
-        'User-Agent' : getToken//REMEBER THIS LINE
+      "User-Agent": getToken //REMEBER THIS LINE
     }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
 }
 
-    request(options, function(err, res, body){
-        cb(err, body)
-    })
-  }
-
-  function downloadImageByURL(url, filePath){
-    request.get(url).pipe(fs.createWriteStream(filePath));
-    }
-
-  
-  getRepoContributors(owner, repoName, function(err, result) {
-   var userArray = JSON.parse(result);
-   if(owner === undefined || repoName === undefined){
-     console.log('Invalid')
-   } else{
-   for(var user of userArray){
-     downloadImageByURL(user.avatar_url, 'avatars/' + user.login + '.jpg')
-   }
-  }
+function downloadImageByURL(url, filePath) {
+  request.get(url).pipe(fs.createWriteStream(filePath));
 }
-  );
 
+getRepoContributors(owner, repoName, function(err, result) {
+  var userArray = JSON.parse(result);
+  if (owner === undefined || repoName === undefined) {
+    console.log("Invalid");
+  } else {
+    for (var user of userArray) {
+      downloadImageByURL(user.avatar_url, "avatars/" + user.login + ".jpg");
+    }
+  }
+});
 
-console.log('Welcome to the GitHub Avatar Downloader!');
+console.log("Welcome to the GitHub Avatar Downloader!");
