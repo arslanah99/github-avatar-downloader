dafc1dd2dbac630fc78c1eff7ea6248852b4615c
arslanah99 <Arah09@hotmail.com>
2019-02-18 06:28:34 +0000

    commented the files

--- download_avatars.js
+++ download_avatars.js
@@ -1,8 +1,11 @@
+//all required variables
 var request = require("request");
 var getToken = require("./secrets");
 var fs = require("fs");
 var owner = process.argv[2];
 var repoName = process.argv[3];
+
+//added ability to select any repo in github
 function getRepoContributors(owner, repoName, cb) {
   var options = {
     url:
@@ -16,15 +19,17 @@ function getRepoContributors(owner, repoName, cb) {
     }
   };
 
+
   request(options, function(err, res, body) {
     cb(err, body);
   });
 }
 
+//grabbing the github avatar images
 function downloadImageByURL(url, filePath) {
   request.get(url).pipe(fs.createWriteStream(filePath));
 }
-
+//adding the images to folder and making an err if the repo or owner is invalid
 getRepoContributors(owner, repoName, function(err, result) {
   var userArray = JSON.parse(result);
   if (owner === undefined || repoName === undefined) {
